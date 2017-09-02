62754838cba2bf0e1c82c9374e5aee7ed6220210
Blake Bertuccelli <blakebertuccelli@gmail.com>
2017-09-02 14:13:49 -0500

    Update Ajax Settings

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -6,7 +6,7 @@ function theme_scripts_and_styles() {
   
   //Styles
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri() );	
-	
+		
 	//Scripts
 	wp_enqueue_script( 'fancybox', get_template_directory_uri() . '/scripts/jquery.fancybox.min.js', array( 'jquery',));
 	wp_enqueue_script( 'theme-navigation', get_template_directory_uri() . '/scripts/themeNavigation.js', array( 'jquery',));
@@ -19,9 +19,9 @@ function theme_scripts_and_styles() {
   wp_enqueue_script( 'google-map', 'http://maps.googleapis.com/maps/api/js?key=AIzaSyBekoSVXQxTDFrLifAGwzl80VllFSsZa14', array( 'jquery' ));
   wp_enqueue_script( 'theme-waypoints',  get_template_directory_uri().'/scripts/themeWaypoints.js', array( 'waypoints' ) );
   wp_enqueue_script( 'waypoints',  get_template_directory_uri().'/scripts/jquery.waypoints.min.js', array( 'jquery' ) );
-
+  
   //Ajax
-  wp_localize_script( 'theme-user_list', 'ajax_params', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+  wp_localize_script( 'theme-filterable_content', 'ajax_params', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts_and_styles' );
