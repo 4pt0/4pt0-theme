abfeb19a7c01fb6c5a4e9e26575b0c0ab833374e
Blake Bertuccelli <blakebertuccelli@gmail.com>
2017-09-02 16:03:00 -0500

    Update User Page to Resolve #8

--- template_parts/layout-user.php
+++ template_parts/layout-user.php
@@ -1,10 +1,37 @@
 <?php
 if(is_author()):
 
-  //Author Variables
+  //User Data
   $user_data = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+
+  //ACF Fields
+  $venture = get_field('venture', 'user_'.$user_data->ID);
   $location = get_field('location', 'user_'.$user_data->ID);
-  $city = explode( "," , $location['address'])[1];
+  $job_title = get_field('job_title', 'user_'.$user_data->ID);
+  $quote = get_field('quote', 'user_'.$user_data->ID);
+  $facebook_url = get_field('facebook_url', 'user_'.$user_data->ID);
+  $twitter_handle = get_field('twitter_handle', 'user_'.$user_data->ID);
+
+  //Location
+  if($location){
+    $city = explode( "," , $location['address'])[1];    
+  }else{
+    $city = null;
+  }
+  
+  //User Meta
+  $first_name = $user_data->first_name;    
+  if(empty($first_name))
+    $first_name = $user_data->display_name;
+
+  //Last Name
+  $last_name = $user_data->last_name;    
+  if(empty($last_name))
+    $last_name = $user_data->display_name;
+  
+  //Bio
+  $bio = get_the_author_meta('description', $user_data->ID)
+ 
 ?>
 
 <div class="layout-user">
@@ -14,56 +41,144 @@ if(is_author()):
 				<div class="avatar-headshot"><?php echo get_avatar( $user_data->ID, 1000 ); ?></div>
 		  </div>
 			<div class= "content-meta">
-				<div class="meta-name"><?php echo $user_data->display_name; ?></div>
+				<div class="meta-name"><?php echo $first_name.' '.$last_name; ?></div>
 				<div class="meta-title_and_venture">
   				
   				<?php
           //Job Title
-          if(get_field('job_title', 'user_'.$user_data->ID))
-  				  echo '<div class="title_and_venture-title">'.get_field('job_title', 'user_'.$user_data->ID).'</div>';
+          if($job_title)
+  				  echo '<div class="title_and_venture-title">'.$job_title.'</div>';
+
+          //Begin Venture
+  				if($venture):
   				?>
-  				<div class="title_and_venture-venture">
-            <a class="venture-link" href="#">Explorable Places</a>
-  				</div>
+          
+          <div class="title_and_venture-venture">
+            
+            <?php
+            //Venture 
+            $post = $venture;
+            setup_postdata( $post ); 
+            echo '<a class="venture-link" href="'.get_permalink().'">'.get_the_title().'</a>';
+            wp_reset_postdata();
+            ?>
+            
+          </div>
+          
+          <?php
+          //End Ventures
+  				endif;
+  				?>
+
         </div>
 				<div class="meta-tags">
-				  <a class="tags-role" href="#">Tiny Alum</a> 
+  				
 				  <?php
   				//Roles
-  				  
+  				if(!empty($user_data->roles)){
+    				foreach( $user_data->roles as $role)
+    			    echo '<div class="tags-role">'.str_replace ('_', ' ', $role).'</div>'; 
+  				}
+  				  				
   				//Location
   				if($location)
-				    echo '<div class="tags-location" href="#">'.$city.'</a>';
+				    echo '<div class="tags-location">'.$city.'</div>';
 				  ?>
+				  
 			  </div>
 		  </div>
 	  </div>
   </div>
-  <div class="user-quote">
-		<div class="quote-body">I applied to tiny because I was excited to get support from area experts around pushing my to the next level.<!-- <?php the_field('quote', $post->ID);?> --></div>
-  </div>
-  <div class="user-bio">
-	  <div class="bio-title">About <?php echo $curauth->first_name; ?></div>
-	  <div class="bio-body">This user set product vision and direction for Explorable Places: a responsive web-based platform for finding field trips aligned to the curriculum. Conducted user testing with teachers and museum professionals to refine product and developed requirements based on user needs. Worked with technical co-founder to develop wireframes, business requirements and UX. Created a strategic marketing and communications plan. Oversaw product launch to facilitate over 400 field trip bookings for nearly 10,000 students in 4 months. Worked with city institutions to build over 25 strategic partnerships and recruit 15 paying customers for our software platform.</div>
-  </div>
+  
+  <?php
+  //Quote
+  if($quote)
+    echo '<div class="user-quote"><div class="quote-body">'.$quote.'</div></div>';
+  
+  //Bio
+  if(!empty($bio))
+   echo '<div class="user-bio"><div class="bio-title">About '.$first_name.'</div><div class="bio-body">'.$bio.'</div></div>';
+  ?>
+  
   <div class="user-footer">
 	  <div class="footer-venture_and_contact">
+			  
+		  <?php
+      //Start Venture
+			if($venture):
+			?>
+
 		  <div class="venture_and_contact-venture">
-			  <div class="venture-heading"><?php echo $user_data->first_name; ?>'s Venture</div>
-				<div class="venture-title">Explorable Places</div>
-				<div class="venture-body">You know getting outside the classroom makes learning stick and creates a lasting impression on your students. But you don't have the time to do all the legwork. At Explorable Places, we've done the legwork for you. Each venue has been vetted by us to be sure there are programs that engage and inspire your students.</div>
-				<a class="venture-url" href="#">Visit Explorableplaces.com</a>
+			  <div class="venture-heading"><?php echo $first_name; ?>'s Venture</div>
+				
+				<?php
+        //Setup Venture Object
+        $post = $venture;
+        setup_postdata( $post ); 
+        ?>
+
+				<div class="venture-title"><?php the_title(); ?></div>
+				<div class="venture-body"><?php the_field('brief_description');?></div>
+				
+				<?php
+        //Venture Website
+        if(get_field('official_website'))
+				  echo '<a class="venture-url" href="'.get_field('official_website').'">Visit '.str_replace ('http://', '', get_field('official_website')).'</a>';
+				
+  		  //Reset Venture Data
+  		  wp_reset_postdata();
+  		  ?>		
+			
 			</div>
+		  
+		  <?php
+      //End Venture
+			endif;
+			
+			//Start Contact Info
+			if(!empty($user_data->user_url) || $twitter_handle || $facebook_url):
+			?>	
+					
 			<div class="venture_and_contact-contact">
-				<div class="contact-title">Contact <?php echo $curauth->first_name; ?></div>
+				<div class="contact-title">Contact <?php echo $first_name; ?></div>
 				<div class="contact-links">
-					<a class="links-website" href="#">http://fakelink.com</a>
+  				
+  				<?php
+          //Website
+          if(!empty($user_data->user_url))
+					  echo '<a class="links-website" href="'.esc_url( $user_data->user_url ).'" target="_blank">'.$user_data->user_url.'</a>';
+				  
+				  //Begin Social
+				  if($twitter_handle || $facebook_url):
+				  ?>
+					
 					<div class="links-social_media">
-						<a class="social_media-link" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a> 
-						<a class="social_media-link" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>  
+  					
+  					<?php
+    				//Twitter
+    				if($twitter_handle)
+						  echo '<a class="social_media-link" href="http://twitter.com/'.str_replace ('@', '', $twitter_handle).'" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>';
+						
+						//Facebook 
+    				if($facebook_url)
+  						echo '<a class="social_media-link" href="'.$facebook_url.'" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>';
+						?>
+						
 				  </div>
+				  
+				  <?php
+          //End Social
+  				endif;
+  				?>
+  				
 				</div>
 			</div>
+			
+			<?php
+  		//End Contact Info
+  		endif;
+  		?>
+  		
 	  </div>    
   </div>
 </div>
