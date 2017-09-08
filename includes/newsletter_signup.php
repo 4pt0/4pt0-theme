1d4d063b4250901fae91c59472192beb8144758c
Blake Bertuccelli <blakebertuccelli@gmail.com>
2017-09-08 16:53:47 -0500

    Update Newsletter Feature to Resolve #47 and #1

--- includes/newsletter_signup.php
+++ includes/newsletter_signup.php
@@ -3,10 +3,10 @@
 function create_newsletter_signup_shortcode() {
 
  
-return '<form class="include-newsletter_signup">
+return '<form class="include-newsletter_signup" action="//4pt0.us2.list-manage.com/subscribe/post?u=302452cbab507fde2dcba4705&amp;id=823a9e1e52">
   <label class="newsletter_singup-label" for="newsletter_signup">Newsletter Signup:</label>
   <div class="newsletter_singup-input_group">
-    <input class="input_group-input" id="newsletter_signup" placeholder="Enter Email" type="text"><button class="input_group-button" type="submit">Subscribe</button>
+    <input class="input_group-input" id="newsletter_signup" placeholder="Enter Email" type="text"  name="EMAIL"><button class="input_group-button" type="submit">Subscribe</button>
   </div>
 </form>';
 
